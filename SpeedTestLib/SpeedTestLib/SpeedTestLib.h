1580de8231dc3cc5ac1bbcc51092a0155f4dbfe9
Denis Gavrilenko <dhaurylenka@exadel.com>
2018-02-05 13:10:43 +0300

    [Init] Add project

---
+++ SpeedTestLib/SpeedTestLib/SpeedTestLib.h
@@ -0,0 +1,19 @@
+//
+//  SpeedTestLib.h
+//  SpeedTestLib
+//
+//  Created by dhaurylenka on 2/5/18.
+//  Copyright © 2018 Exadel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpeedTestLib.
+FOUNDATION_EXPORT double SpeedTestLibVersionNumber;
+
+//! Project version string for SpeedTestLib.
+FOUNDATION_EXPORT const unsigned char SpeedTestLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeedTestLib/PublicHeader.h>
+
+
